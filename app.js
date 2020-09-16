29b6495b07a23e5b4cbd95dbaba7f9c90404f37d
Asa Tirsen <asa.tirsen@gmail.com>
2020-09-16 12:25:59 +0200

    Gets reports if linked clicked encryption works. getReport fn added

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const index = require('./routes/index');
+const reports = require('./routes/reports');
+const register = require('./routes/register')
+const login = require('./routes/login')
+
 
 const port = 1337;
 
@@ -12,10 +16,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/', index);
+app.use('/reports', reports);
+app.use('/register', register);
+app.use('/login', login);
+
 
 app.use((req, res, next) => {
-    console.log(req.method);
-    console.log(req.path);
     next();
 });
 
@@ -36,44 +42,63 @@ if (process.env.NODE_ENV !== 'test') {
 // });
 
 // Testing routes with method
-app.get("/user", (req, res) => {
-    res.json({
-        data: {
-            msg: "Got a GET request, sending back default 200"
-        }
-    });
-});
+// app.get("/user", (req, res) => {
+//     res.json({
+//         data: {
+//             msg: "Got a GET request, sending back default 200"
+//         }
+//     });
+// });
+//
+// app.post("/register", (req, res) => {
+//     res.status(201).json({
+//         data: {
+//             msg: "Got a POST request, sending back 201 Created"
+//         }
+//     });
+// });
+//
+// app.put("/user", (req, res) => {
+//     res.status(204).send();
+// });
 
-app.post("/user", (req, res) => {
-    res.status(201).json({
-        data: {
-            msg: "Got a POST request, sending back 201 Created"
-        }
-    });
-});
-
-app.put("/user", (req, res) => {
-    res.status(204).send();
-});
-
-app.delete("/user", (req, res) => {
-    res.status(204).send();
-});
-
-app.get("/hello/:msg", (req, res) => {
-    const data = {
-        data: {
-            msg: req.params.msg
-        }
-    };
-
-    res.json(data);
-});
+// app.delete("/user", (req, res) => {
+//     res.status(204).send();
+// });
+//
+// app.get("/hello/:msg", (req, res) => {
+//     const data = {
+//         data: {
+//             msg: req.params.msg
+//         }
+//     };
+//
+//     res.json(data);
+// });
 
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+
+// app.use((err, req, res, next) => {
+//     if (res.headersSent) {
+//         return next(err);
+//     }
+//
+//     res.status(err.status || 500).json({
+//         "errors": [
+//             {
+//                 "status": err.status,
+//                 "title":  err.message,
+//                 "detail": err.message
+//             }
+//         ]
+//     });
+// });
+
+// Start up server
+app.listen(port, () => console.log(`Example API listening on port ${port}!`));
 // Add routes for 404 and error handling
 // Catch 404 and forward to error handler
 // Put this last
@@ -82,22 +107,3 @@ app.use((req, res, next) => {
     err.status = 404;
     next(err);
 });
-
-app.use((err, req, res, next) => {
-    if (res.headersSent) {
-        return next(err);
-    }
-
-    res.status(err.status || 500).json({
-        "errors": [
-            {
-                "status": err.status,
-                "title":  err.message,
-                "detail": err.message
-            }
-        ]
-    });
-});
-
-// Start up server
-app.listen(port, () => console.log(`Example API listening on port ${port}!`));
--- db/migrate.sql
+++ db/migrate.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS users (
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    UNIQUE(email)
+CREATE TABLE IF NOT EXISTS users
+(
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(60)  NOT NULL,
+    UNIQUE (email)
 );
 
-
-CREATE TABLE IF NOT EXISTS test (
-    testColumn VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS texts (
-    id  INTEGER PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS texts
+(
+    id       INTEGER PRIMARY KEY,
+    week     INTEGER       NULL,
+    title    VARCHAR(255)  NOT NULL,
     longtext VARCHAR(3000) NOT NULL
 );
+
+-- DELETE FROM texts WHERE id=2;
