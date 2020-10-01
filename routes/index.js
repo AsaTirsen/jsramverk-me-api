283767268f00e819819bad036f7d1ba1fed33d01
Asa Tirsen <asa.tirsen@gmail.com>
2020-10-01 12:24:10 +0200

    Changes to routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const texts = require('../models/texts')
 
-
 router.get('/', function (req, res, next) {
     texts.getText(res, "Lite om mig");
 });
