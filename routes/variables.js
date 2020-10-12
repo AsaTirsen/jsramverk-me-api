365892061a6955427a2e718772efdfbda9aeebe1
Asa Tirsen <asa.tirsen@gmail.com>
2020-10-12 11:17:33 +0200

    removed condition for envVars

--- routes/variables.js
+++ routes/variables.js
@@ -1,16 +1,7 @@
 const dotenv = require('dotenv');
-const envVars = dotenv.config();
+dotenv.config();
 
-if (envVars) {
-    module.exports = {
-        secret: process.env.JWT_SECRET,
-        port: process.env.PORT
-    };
-    console.log(envVars);
-} else {
-    module.exports = {
-        secret: "min_hemlis",
-        port: 1337
-    }
-    console.log(secret)
-}
+module.exports = {
+    secret: process.env.JWT_SECRET,
+    port: process.env.PORT
+};
